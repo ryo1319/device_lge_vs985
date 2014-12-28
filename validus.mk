db70ecf71a9819d177ea06aa9715c2fce83f22f2
Adam <ovadia.adam@gmail.com>
2014-12-28 08:59:19 -0500

    Update and rename cm.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_vs985
+PRODUCT_NAME := validus_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
